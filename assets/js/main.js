ed08126980c5857da81bad6069da3fb87e8f59bd
Gyzmou <gyzmou4243@gmail.com>
2025-03-27 00:13:43 +0100

    modification staff nouveau menu deroulant , plusieur modif sur le sit en general

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,18 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 200}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 50}); 
+
+
+
+function toggleDropdown(id) {
+    const dropdown = document.getElementById(id);
+    const toggleButton = dropdown.previousElementSibling.querySelector('.dropdown__toggle');
+
+    if (dropdown.classList.contains('open')) {
+        dropdown.classList.remove('open'); // Ferme le menu
+        toggleButton.classList.remove('open'); // Réinitialise la flèche
+    } else {
+        dropdown.classList.add('open'); // Ouvre le menu
+        toggleButton.classList.add('open'); // Tourne la flèche
+    }
+}
